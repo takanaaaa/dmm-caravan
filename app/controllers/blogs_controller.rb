51defa35f84392858ddd72f3ba3317805a1aae88
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-05 04:16:56 +0000

    [Finish]3-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,9 +4,12 @@ class BlogsController < ApplicationController
   end
 
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blog_path(blog.id)
+    @blog = Blog.new(blog_params)
+    if @blog.save
+    redirect_to blog_path(@blog.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -18,9 +21,12 @@ class BlogsController < ApplicationController
   end
   
   def update
-    blog = Blog.find(params[:id])
-    blog.update(blog_params)
-    redirect_to blog_path(blog)
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blog_path(@blog)
+    else
+      render :edit
+    end
   end
 
   def show
